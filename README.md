a6e40b3e7d0c38feb95c7e974a104091b7d7025c
Chum4n <chuman.fartojv@gmail.com>
2022-07-14 10:59:24 -0300

    Projeto Finalizado

---
+++ README.md
@@ -0,0 +1,7 @@
+# Projeto Meus Links(Com temas)
+
+##### João Vitor Chuman Farto Fernandes
+
+Projeto desenvolvido em HTML, CSS e JavaScript. Juntando duas coisas que gosto, front-end e Futebol, colocando Times que gosto como temas do desenvolvimento.
+
+​	
---
+++ script.js
@@ -0,0 +1,30 @@
+document.body.setAttribute('class', 'vasco');
+
+let btnVasco      = document.querySelector('.btn-tm-vasco');
+let btnPalmeiras  = document.querySelector('.btn-tm-palmeiras');
+let btnLinense    = document.querySelector('.btn-tm-linense ')
+let btnLondrina   = document.querySelector('.btn-tm-londrina');
+
+btnVasco.addEventListener('click', () =>{
+    if(document.getElementsByClassName('')){
+        document.body.setAttribute('class', 'vasco');
+    }
+});
+
+btnPalmeiras.addEventListener('click', () =>{
+    if(document.getElementsByClassName('')){
+        document.body.setAttribute('class', 'palmeiras');
+    }
+});
+
+btnLinense.addEventListener('click', () =>{
+    if(document.getElementsByClassName('')){
+        document.body.setAttribute('class', 'linense');
+    }
+});
+
+btnLondrina.addEventListener('click', () =>{
+    if(document.getElementsByClassName('')){
+        document.body.setAttribute('class', 'londrina');
+    }
+});
